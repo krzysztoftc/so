438ae5ef04bba534a4a8548cfc377cd3e3967910
Krzysztof Cabala <210047@student.pwr.wroc.pl>
2016-06-02 07:01:49 +0200

    so many changes

---
+++ src/mutex.h
@@ -0,0 +1,8 @@
+//
+// Created by krzysztof on 30.05.16.
+//
+
+#ifndef NCURSES_MUTEX_H
+#define NCURSES_MUTEX_H
+
+#endif //NCURSES_MUTEX_H
---
+++ src/place.h
@@ -0,0 +1,8 @@
+//
+// Created by krzysztof on 30.05.16.
+//
+
+#ifndef NCURSES_PLACE_H
+#define NCURSES_PLACE_H
+
+#endif //NCURSES_PLACE_H
